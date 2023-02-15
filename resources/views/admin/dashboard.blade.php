ae57390f77720f5b934bbdbd002dac819de81563
aditya darma <bagusaditya1899@gmail.com>
2023-02-16 02:55:01 +0800

    role

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,31 +5,31 @@
 @section('content')
     <div class="row">
         <div class="col-lg-12 mb-4 order-0">
-            @if ($data->id_pembina == null && Auth::user()->roles == 'user')
-            <div class="card">
-                <div class="d-flex align-items-end row">
-                    <div class="col-sm-7">
-                        <div class="card-body">
-                            <h5 class="card-title text-primary">Congratulations John! 🎉</h5>
-                            <p class="mb-4">
-                                Kamu Harus mengupdate Data Diri dan <span class="fw-bold">Password</span> Agar lebih aman
-                            </p>
-                            <a href="" class="btn btn-sm btn-outline-primary">Update Profile & Password?</a>
+            @if (null === ($data ?? null) && Auth::user()->roles == 'user')
+                <div class="card">
+                    <div class="d-flex align-items-end row">
+                        <div class="col-sm-7">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Congratulations John! 🎉</h5>
+                                <p class="mb-4">
+                                    Kamu Harus mengupdate Data Diri dan <span class="fw-bold">Password</span> Agar lebih aman
+                                </p>
+                                <a href="" class="btn btn-sm btn-outline-primary">Update Profile & Password?</a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-sm-5 text-center text-sm-left">
-                        <div class="card-body pb-0 px-0 px-md-4">
-                            <img src="../assets/img/illustrations/man-with-laptop-light.png" height="140"
-                                alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png"
-                                data-app-light-img="illustrations/man-with-laptop-light.png" />
+                        <div class="col-sm-5 text-center text-sm-left">
+                            <div class="card-body pb-0 px-0 px-md-4">
+                                <img src="../assets/img/illustrations/man-with-laptop-light.png" height="140"
+                                    alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                                    data-app-light-img="illustrations/man-with-laptop-light.png" />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             @endif
             <div class="card p-2 my-2">
                 <div class="row">
-                    @if (Auth::user()->roles == 'admin' )
+                    @if (Auth::user()->roles == 'admin')
                         @include('admin.layouts.dashboard.admin-dashboard')
                     @elseif (Auth::user()->roles == 'pembina')
                         @include('admin.layouts.dashboard.pembina-dashboard')
