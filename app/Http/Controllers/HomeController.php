d101c696d3b93ea92fab8a61004eb0b1fef01588
aditya darma <bagusaditya1899@gmail.com>
2023-02-17 17:44:48 +0800

    asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $magangs = Magang::limit(10)->latest()->get();
+        $magangs = Magang::latest()->limit(10)->get();
         return view('home', compact('magangs'));
     }
 }
