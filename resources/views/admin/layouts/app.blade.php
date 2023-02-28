28c8707bef28e6c267586165c763c2e88bb4f053
baharma <bagusaditya1899@gmail.com>
2023-02-28 23:42:45 +0800

    errors

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -54,8 +54,8 @@
     @if (session('message'))
         <span class="success" data-message="{{ session('message') }}"></span>
     @endif
-    @if (session('errors'))
-    <span class="error" data-message="{{ session('errors') }}"></span>
+    @if (session('error'))
+    <span class="error" data-message="{{ session('error') }}"></span>
 @endif
 
     <!-- Layout wrapper -->
